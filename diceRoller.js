7e562cc3c59e809cb4f16bef76251d0b37ab6d5a
sluchie88 <55814053+sluchie88@users.noreply.github.com>
2019-10-07 18:14:29 -0500

    Fixed errors in code and logic. Need CSS fixes
    Code runs well and results are displaying correctly. Issues that still need fixing are CSS and styling. Buttons not appearing in right place, input field and its label are still crammed together.

--- diceRoller.js
+++ diceRoller.js
@@ -36,14 +36,14 @@ var rollCount = 0;
 //Rolls dice for the game
 //Checks if result is 7 or not and returns to playLucky7s
 function rollDice(){
-	console.log("rollDice called");
+	//console.log("rollDice called");
 	rollCount++;
 	var dice1 = Math.floor(Math.random() *6) +1;
 	var dice2 = Math.floor(Math.random() *6) +1;
 	var diceAmt = dice1 + dice2;
-	console.log("Rolled for dice 1: " + dice1 
-	+ ". Rolled for dice2: " + dice2 +
-	 ". Total of both dice: " + diceAmt);
+	//console.log("Rolled for dice 1: " + dice1 
+	//+ ". Rolled for dice2: " + dice2 +
+	// ". Total of both dice: " + diceAmt);
 	if ((dice1 + dice2) == 7){
 		return true;
 	}
@@ -58,35 +58,57 @@ Checks amount of money
 adds and subtracts from startingBet according to dice results
 tracks dice rolls and amount won throughout game (move to separate functions?)*/
 function playLucky7s(){
-	console.log("playLucky7s called");
+	//console.log("playLucky7s called");
 	var startingBet = document.forms["lucky7s"]["startingBet"].value;
-	console.log(startingBet);
+	var startingBetOriginal = startingBet;
+	console.log("The value of startingBet is: " + startingBet);
 	var highestAmountWonRollCount = 0;
 	var highestAmountWon = 0;
-	if(startingBet != 0){
+		if(startingBet == 0 || startingBet == ""){
+			alert("Please enter a number higher than zero.");
+		}
+	else{
 		while (startingBet > 0){
 			if(rollDice() == true){
 				startingBet = startingBet + 4;
 				highestAmountWonRollCount = rollCount;
 					if(startingBet > highestAmountWon){
 						highestAmountWon = startingBet;
-						console.log("The highest amount won so far is: " + highestAmountWon + 
-						". The current highestAmountWonRollCount is: " + highestAmountWonRollCount);
+						//console.log("The highest amount won so far is: " + highestAmountWon + 
+						//". The current highestAmountWonRollCount is: " + highestAmountWonRollCount);
 					}
 			}
 			else{
 				startingBet--;
-				console.log("Lost. Current dice roll: " + rollCount + ". Currently have: " + startingBet);
+				//console.log("Lost. Current dice roll: " + rollCount + ". Currently have: " + startingBet);
 			}
 		}
 	}
-	else{
-		alert("Please enter a number higher than zero.");
-	}
+	document.getElementById("starting").innerText = startingBetOriginal;
+	document.getElementById("totalRolls").innerText = rollCount;
+	document.getElementById("highestAmountWon").innerText = highestAmountWon;
+	document.getElementById("highestAmountWonRollCount").innerText = highestAmountWonRollCount;
+	displayElements();
 	return false;
 }
 
-function resetGame(){
-	document.forms["lucky7s"]["startingBet"].value = "";
-	document.getElementById("results").style.display = "block";
+
+function displayElements(){
+	//console.log("Display function called");
+	var tableResults = document.getElementById("results");
+	var buttonDisplay = document.getElementById("playAgainButton");
+	if (tableResults.style.display === "none"){
+		//console.log("Stuff should be displaying now");
+		tableResults.style.display = "block";
+		//console.log("The current display value of tableResults is: " + tableResults.style.display);
+		buttonDisplay.style.display = "block";
+		//console.log("The current display value of tableResults is: " + buttonDisplay.style.display);
+		return false;
+	}
+	else {
+		tableResults.style.display = "none";
+		buttonDisplay.style.display = "none";
+		rollCount = 0;
+		return false;
+	}
 }
